a54cc7951fa0c20f736e9cbb12a8d9d796264f7f
SaluMel <samuel.reginaldo@escola.pr.gov.br>
2026-08-13 08:15:05 -0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = mysql.createPoll({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
