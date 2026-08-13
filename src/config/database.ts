c83db6d8a987fe855bfe217cfbec0ef76da6f764
SaluMel <samuel.reginaldo@escola.pr.gov.br>
2026-08-13 08:17:54 -0300

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,7 +8,7 @@ export const pool = mysql.createPool({
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  databas: process.env.DB_NAME,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
